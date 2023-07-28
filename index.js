62fad79cf96cf399fb9ba69d9a5b2567760ad089
Shubham <srivastavashubham2003@gmail.com>
2023-07-28 11:40:01 +0530

    Index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT ||8080
+
+app.use("/",(req,res)=>{
+  res.send("This is live ");
+});
+
+
+app.listen(5000, () => {
+  console.log(`App running on port ${PORT}`);
+})
+
